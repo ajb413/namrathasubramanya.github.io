e0c6c55f5fe962fdfeb1e65d3e176029b40e0198
namrathasubramanya <namratha.subramanya@gmail.com>
2018-02-19 02:24:15 -0800

    Update dogwalker.js
    Comments added

--- dogwalker.js
+++ dogwalker.js
@@ -1,27 +1,40 @@
+// PubNub utilizes a Publish/Subscribe model for real-time data streaming and device signaling which
+// lets you establish and maintain persistent socket connections to any device and push data to global 
+// audiences in less than ¼ of a second. You can publish messages to any given channel, and subscribing 
+// clients receive only messages associated with that channel. The message payload can be any JSON data 
+// including numbers, strings, arrays, and objects.
+
+// You will need at the minimum a subscribeKey and publishKey. If a client will only subscribe, and not publish, 
+// then the client only need to initialize with the subscribeKey. For clients who will be publishing only, or 
+// publishing and subscribing (a client can both publish and subscribe), it will need to initialize with both 
+// the subscribeKey and the publishKey.
+
+
 var pubnub = new PubNub({
-    subscribeKey: 'sub-c-0cd4f18e-11d5-11e8-b32f-5ea260837941', 
-    publishKey: 'pub-c-146542ff-637d-43fa-a28a-cad0dbaf697e' 
+    subscribeKey: 'sub-c-0cd4f18e-11d5-11e8-b32f-5ea260837941', // Enter your subscribe key here
+    publishKey: 'pub-c-146542ff-637d-43fa-a28a-cad0dbaf697e' // Enter your publish key here
   });
+
 function success(position) {
   var s = document.querySelector('#status');
-
   if (s.className == 'success') {
-    
     return;
   }
 
   s.innerHTML = " ";
   s.className = 'success';
-
+    
+  // create map using javascript
   var mapcanvas = document.createElement('div');
   mapcanvas.id = 'mapcanvas';
   mapcanvas.style.height = '400px';
   mapcanvas.style.width = '560px';
 
   document.querySelector('article').appendChild(mapcanvas);
-
+  
   var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  console.log(position.coords.latitude);
+  
+  // map 
   var myOptions = {
     zoom: 15,
     center: latlng,
@@ -30,14 +43,17 @@ function success(position) {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("mapcanvas"), myOptions);
-
+  
+  // map marker
   var marker = new google.maps.Marker({
       position: latlng,
       map: map,
       title:""
   });
 
-  
+  // You need to supply the publishKey to clients that will publish (send) data to the application over 
+  // the PubNub network.
+    
   pubnub.publish(
     {
         message: {
@@ -67,6 +83,8 @@ function error(msg) {
   s.className = 'fail';
 
 }
+
+// Geolocation API
 
 if (navigator.geolocation) {
   navigator.geolocation.watchPosition(success, error, options);
