42a45ff1a7f17943e412d554089fac499f068865
namrathasubramanya <namratha.subramanya@gmail.com>
2018-02-19 02:33:54 -0800

    Update dogowner.js
    Comments added

--- dogowner.js
+++ dogowner.js
@@ -1,7 +1,17 @@
+// PubNub utilizes a Publish/Subscribe model for real-time data streaming and device signaling which
+// lets you establish and maintain persistent socket connections to any device and push data to global 
+// audiences in less than ¼ of a second. You can publish messages to any given channel, and subscribing 
+// clients receive only messages associated with that channel. The message payload can be any JSON data 
+// including numbers, strings, arrays, and objects.
+
+// You will need at the minimum a subscribeKey and publishKey. If a client will only subscribe, and not publish, 
+// then the client only need to initialize with the subscribeKey. For clients who will be publishing only, or 
+// publishing and subscribing (a client can both publish and subscribe), it will need to initialize with both 
+// the subscribeKey and the publishKey.
 
 var pubnub = new PubNub({
-  subscribeKey: 'sub-c-0cd4f18e-11d5-11e8-b32f-5ea260837941', 
-  publishKey: 'pub-c-146542ff-637d-43fa-a28a-cad0dbaf697e' 
+  subscribeKey: 'sub-c-0cd4f18e-11d5-11e8-b32f-5ea260837941', // Enter your subscribe key here
+  publishKey: 'pub-c-146542ff-637d-43fa-a28a-cad0dbaf697e' // Enter your publish key here
 });
 
 function initMap() {
@@ -11,21 +21,24 @@ function initMap() {
     message: function(message){
       latValue = message.message.latitude;
       longValue = message.message.longitude;
-        console.log(message.message.latitude);
-         console.log(message.message.longitude);
+       
+      // initialise map
         var map = new google.maps.Map(document.getElementById('map'), {
            zoom: 15,
            center: {lat: latValue, lng: longValue}
         });
         var geocoder = new google.maps.Geocoder;
         var infowindow = new google.maps.InfoWindow;
+      
         function geocodeLatLng(geocoder, map, infowindow, latValue, longValue) {
           var latlng = {lat: latValue, lng: longValue};
-          console.log('latValue'+latValue);
+          
           geocoder.geocode({'location': latlng}, function(results, status) {
             if (status === 'OK') {
               if (results[0]) {
                 map.setZoom(15);
+                
+                // map marker
                 var marker = new google.maps.Marker({
                   position: latlng,
                   map: map
@@ -41,13 +54,13 @@ function initMap() {
           });
         }
 
-  console.log("latitude value", latValue);
-  console.log("longitude value", longValue);
   geocodeLatLng(geocoder, map, infowindow, latValue, longValue);
 
       }
     });
 
+    // subscribe to the channel that is published through PubNub publish()
+  
     pubnub.subscribe({
         channels: ['walkmydog']
     });
